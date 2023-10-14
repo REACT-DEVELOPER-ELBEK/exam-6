13cce817cf141192235decb7c155a0784a928db4
Yusupov Elbek <elbeky829@gmail.com>
2023-10-14 23:25:06 +0500

    test-2

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     } else {
       localStorage.setItem("userName", JSON.stringify(userName));
       localStorage.setItem("userPassword", JSON.stringify(password));
-      goProfile("/profile");
+      goProfile("");
       setTimeout(() => {
         window.location.reload();
       }, 1400);
